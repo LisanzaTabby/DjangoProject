5059f56080b51639bc2f592f0b0911baa3479539
Tabitha-Lisanza <147367069+LisanzaTabby@users.noreply.github.com>
2024-06-06 16:28:08 +0300

    update urls.py

--- smsApp/urls.py
+++ smsApp/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
-    #path('add_user/', views.add_user, name='add_user'),
     path('users/', views.list_users, name='list_users'),
-    path('edit/', views.edit, name='edit'),
     path('signout', views.signout, name= 'signout'),
 ]
